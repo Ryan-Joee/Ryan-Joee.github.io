9de4a0dfb8e23fcd77c1a82d2e0659fb58ce16d1
Ryan-Joee <3497882088@qq.com>
2025-01-13 18:26:11 +0800

    Site updated: 2025-01-13 18:26:11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["Ryan/这是另一篇新的文章/","Ryan/hello-world/"];function toRandomPost(){
+var posts=["Ryan/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
